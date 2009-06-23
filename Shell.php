2e43d6416f05c4d1ba8a09a265918ff0be01bb26
sant0sk1 <sant0sk1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-23 17:45:26 +0000

    added license and readme. customized color scheme.
    git-svn-id: http://svn.wp-plugins.org/wordpress-console/trunk@128834 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Shell.php
+++ Shell.php
@@ -136,8 +136,8 @@ class PHP_Shell {
         $cmd = PHP_Shell_Commands::getInstance();
 
         $cmd->registerCommand('#^quit$#', $this, 'cmdQuit', 'quit', 'leaves the shell');
+        $cmd->registerCommand('#^exit$#', $this, 'cmdQuit', 'exit', 'leaves the shell');
         $cmd->registerCommand('#^\?$#', $this, 'cmdHelp', '?', 'show this help');
-        $cmd->registerCommand('#^\?\s+license$#', $this, 'cmdLicense', '? license', 'show license of the shell');
     }
 
    
@@ -762,38 +762,7 @@ class PHP_Shell {
 
         return var_export($o, 1);
     }
-
-    /**
-    * get the license string
-    *
-    * @return string the inline help as string 
-    */
-    public function cmdLicense($l) {
-        $o = <<<EOF
-(c) 2006 Jan Kneschke <jan@kneschke.de>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-EOF;
-
-        return var_export($o, 1);
-    }
-
+    
     /**
     * handle the 'quit' command
     *
@@ -1078,5 +1047,3 @@ function __shell_readline_complete($str, $pos) {
     # printf("%s ... %s\n", $str, $pos);
     return $m;
 }
-
-
