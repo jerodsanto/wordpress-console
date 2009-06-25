59dd2cca567fe8cc67e3a9a518a9269e8df9022e
sant0sk1 <sant0sk1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-25 00:19:37 +0000

    bump patch version
    git-svn-id: http://svn.wp-plugins.org/wordpress-console/trunk@129231 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-console.php
+++ wordpress-console.php
@@ -5,7 +5,7 @@ Plugin URI: http://github.com/sant0sk1/wordpress-console
 Description: An interactive console for WordPress developers
 Author: Jerod Santo
 Author URI: http://jerodsanto.net
-Version: 0.1.0
+Version: 0.1.1
 */
 
 // THE CSS AND THE JAVASCRIPTS
