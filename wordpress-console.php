4bdd136504fa95f1b8f7af40761924153f0debab
sant0sk1 <sant0sk1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-04 21:59:49 +0000

    OO
    git-svn-id: http://svn.wp-plugins.org/wordpress-console/trunk@260256 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-console.php
+++ wordpress-console.php
@@ -8,46 +8,62 @@ Author URI: http://jerodsanto.net
 Version: 0.2.3
 */
 
-define( 'WP_CONSOLE_URL', WP_PLUGIN_URL . "/wordpress-console/" );
+class WordPressConsole {
+  public  $version;
+  private $url;
+  private $secret;
 
-// THE CSS AND THE JAVASCRIPTS
-function console_admin_css() {
-  wp_enqueue_style( 'console', WP_CONSOLE_URL . "console.css" );
-}
+  function __construct() {
+    $this->version = '0.2.3';
+    $this->url     = WP_PLUGIN_URL . "/wordpress-console/";
+    $this->secret  = $this->get_secret();
 
-function console_admin_javascripts() {
-  wp_enqueue_script( 'console', WP_CONSOLE_URL . 'console.js', array( 'jquery' ) );
-  wp_enqueue_script( 'sha1', WP_CONSOLE_URL . 'sha1.js', array( 'jquery' ) );
-}
-
-// THE CONSOLE
-function console_admin_page() {
-  $secret = get_option( 'wordpress-console-secret' );
-  if ( !$secret ) {
-    $secret = md5( time() . php_uname("n") . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_HOST'] . __FILE__ );
-    update_option( 'wordpress-console-secret', $secret );
+    add_action( 'admin_menu', array( &$this, 'init' ) );
   }
-  ?>
-  <script type="text/javascript" charset="utf-8">
-    var WP_CONSOLE_VERSION = <?php echo json_encode( '0.2.3' )  ?>;
-    var WP_CONSOLE_URL     = <?php echo json_encode( WP_CONSOLE_URL ) ?>;
-    var WP_CONSOLE_SECRET  = <?php echo json_encode( $secret ) ?>;
-  </script>
-  <div class="wrap">
-    <h2>WordPress Console: "?" for help menu</h2>
-    <div id="wrapper">
-    </div>
-  </div>
-  <?php
+
+  function init() {
+    $page = add_menu_page( 'Console', 'Console', 10, 'console',
+      array( &$this, 'admin' ), $this->url . "icon16.png" );
+    add_action( "admin_print_scripts-$page", array( &$this, 'scripts' ) );
+    add_action( "admin_print_styles-$page", array( &$this, 'styles' ) );
+  }
+
+  function admin() { ?>
+    <script type="text/javascript" charset="utf-8">
+      var WP_CONSOLE_VERSION = <?php echo json_encode( $this->version ) ?>;
+      var WP_CONSOLE_URL     = <?php echo json_encode( $this->url )     ?>;
+      var WP_CONSOLE_SECRET  = <?php echo json_encode( $this->secret )  ?>;
+    </script>
+    <div class="wrap">
+      <h2>WordPress Console: "?" for help menu</h2>
+      <div id="wrapper">
+      </div>
+    </div><?php
+  }
+
+  function scripts() {
+    wp_enqueue_script( 'console', $this->url . 'console.js', array( 'jquery' ) );
+    wp_enqueue_script( 'sha1', $this->url . 'sha1.js', array( 'jquery' ) );
+  }
+
+  function styles() {
+    wp_enqueue_style( 'console', $this->url . "console.css" );
+  }
+
+  private function get_secret() {
+    if ( $secret = get_option( 'wordpress-console-secret' ) ) {
+      return $secret;
+    }
+
+    $secret = md5( time() * time() );
+    update_option( 'wordpress-console-secret', $secret );
+    return $secret;
+  }
+
+  function WordPressConsole() { // legacy
+    $this->__construct();
+  }
 }
 
-// THE HOOK-UP
-function console_hooks() {
-  $page = add_menu_page( 'Console', 'Console', 10, 'console',
-    'console_admin_page', WP_CONSOLE_URL . "icon16.png" );
-  add_action("admin_print_scripts-$page",'console_admin_javascripts');
-  add_action("admin_print_styles-$page", 'console_admin_css');
-}
-
-add_action('admin_menu', 'console_hooks');
+new WordPressConsole;
 ?>
