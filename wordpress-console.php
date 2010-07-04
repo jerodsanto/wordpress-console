ac1a9cbd8fa85805951d2ce52e2f68b93e2c35ce
sant0sk1 <sant0sk1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-04 21:59:42 +0000

    top billing
    git-svn-id: http://svn.wp-plugins.org/wordpress-console/trunk@260255 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-console.php
+++ wordpress-console.php
@@ -8,30 +8,32 @@ Author URI: http://jerodsanto.net
 Version: 0.2.3
 */
 
+define( 'WP_CONSOLE_URL', WP_PLUGIN_URL . "/wordpress-console/" );
+
 // THE CSS AND THE JAVASCRIPTS
 function console_admin_css() {
-  wp_enqueue_style('console',WP_PLUGIN_URL . "/wordpress-console/console.css");
+  wp_enqueue_style( 'console', WP_CONSOLE_URL . "console.css" );
 }
 
 function console_admin_javascripts() {
-  wp_enqueue_script('console', WP_PLUGIN_URL . '/wordpress-console/console.js', array('jquery'));
-  wp_enqueue_script('sha1', WP_PLUGIN_URL . '/wordpress-console/sha1.js', array('jquery'));
+  wp_enqueue_script( 'console', WP_CONSOLE_URL . 'console.js', array( 'jquery' ) );
+  wp_enqueue_script( 'sha1', WP_CONSOLE_URL . 'sha1.js', array( 'jquery' ) );
 }
 
 // THE CONSOLE
 function console_admin_page() {
-  $secret = get_option('wordpress-console-secret');
+  $secret = get_option( 'wordpress-console-secret' );
   if ( !$secret ) {
     $secret = md5( time() . php_uname("n") . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_HOST'] . __FILE__ );
-    update_option('wordpress-console-secret', $secret);
+    update_option( 'wordpress-console-secret', $secret );
   }
   ?>
   <script type="text/javascript" charset="utf-8">
     var WP_CONSOLE_VERSION = <?php echo json_encode( '0.2.3' )  ?>;
-    var WP_CONSOLE_URL     = <?php echo json_encode( WP_PLUGIN_URL . '/wordpress-console/' ) ?>;
+    var WP_CONSOLE_URL     = <?php echo json_encode( WP_CONSOLE_URL ) ?>;
     var WP_CONSOLE_SECRET  = <?php echo json_encode( $secret ) ?>;
   </script>
-  <div id="wrap">
+  <div class="wrap">
     <h2>WordPress Console: "?" for help menu</h2>
     <div id="wrapper">
     </div>
@@ -41,7 +43,8 @@ function console_admin_page() {
 
 // THE HOOK-UP
 function console_hooks() {
-  $page = add_management_page('Console', 'Console', 10, __FILE__, 'console_admin_page');
+  $page = add_menu_page( 'Console', 'Console', 10, 'console',
+    'console_admin_page', WP_CONSOLE_URL . "icon16.png" );
   add_action("admin_print_scripts-$page",'console_admin_javascripts');
   add_action("admin_print_styles-$page", 'console_admin_css');
 }
