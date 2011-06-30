8687818177f6354eba36ddb10f278d798ce38df6
Johnny Lim <lim.johnny88@gmail.com>
2011-07-01 03:09:18 +0800

    Add show parameters functionality reload() will now unset $_SESSION['code']

--- reload.php
+++ reload.php
@@ -7,6 +7,8 @@ if (isset($_SESSION['console_vars'])) {
 if (isset($_SESSION['partial'])) {
   unset($_SESSION['partial']);
 }
-
+if (isset($_SESSION['code'])) {
+  unset($_SESSION['code']);
+}
 print json_encode(array('output' => "Success!"));
 ?>
