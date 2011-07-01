8997b36629b2dca0cc4a91ac37ea6277bc2fb2bf
Johnny Lim <lim.johnny88@gmail.com>
2011-07-01 23:12:22 +0800

    Revert back

--- wordpress-console.php
+++ wordpress-console.php
@@ -62,11 +62,6 @@ class WordPressConsole {
     <div class="wrap">
       <h2>WordPress Console: "?" for help menu</h2>
       <div id="wrapper">
-		<div id="shell_control_panel">
-			<div class="shell_controls">
-				<label>Show parameter: <input id="shell_show_parameter_chkbx" type="checkbox" name="show_parameter" /></label>
-			</div>
-		 <div>
       </div>
     </div><?php
   }
